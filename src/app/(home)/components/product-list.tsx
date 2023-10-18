0a1b205abd35d8fba1b59c613002120c5e2e63b0
felipprodrigues <dev.rodriguesfelipp@gmail.com>
2023-10-18 15:23:41 -0300

    feat: add product items and product components

--- src/app/(home)/components/product-list.tsx
+++ src/app/(home)/components/product-list.tsx
@@ -1,4 +1,5 @@
-import ProductItem from "@/components/ui/product.item";
+import ProductItem from "@/components/ui/product-item";
+import { computeProductTotalPrice } from "@/helpers/product";
 
 import { Product } from "@prisma/client";
 
@@ -10,7 +11,10 @@ export default async function ProductList({ products }: ProductListProps) {
   return (
     <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
       {products.map((product) => (
-        <ProductItem key={product.id} product={product} />
+        <ProductItem
+          key={product.id}
+          product={computeProductTotalPrice(product)}
+        />
       ))}
     </div>
   );
