9b08e24c0f28798ee821df6d2072709b61ef418c
felipprodrigues <dev.rodriguesfelipp@gmail.com>
2023-10-19 21:55:24 -0300

    feat: add discount and

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -4,28 +4,69 @@ import { CartContext } from "@/providers/cart";
 import { useContext } from "react";
 import { CartItem } from "./cart-item";
 import { computeProductTotalPrice } from "@/helpers/product";
+import { Separator } from "./separator";
+import { ScrollArea } from "./scroll-area";
+import { Button } from "./button";
 
 export function Cart() {
-  const { products } = useContext(CartContext);
+  const { products, total, subTotal, totalDiscount } = useContext(CartContext);
 
   console.log(products, "aqui");
   return (
-    <div className=" flex flex-col gap-8">
+    <div className=" flex h-full flex-col gap-8">
       <Badge
         className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
         variant="outline"
       >
         <ShoppingCartIcon size={16} />
-        Catálogo
+        Carrinho
       </Badge>
 
-      <div className="flex flex-col gap-5">
-        {products.map((product) => (
-          <CartItem
-            product={computeProductTotalPrice(product as any) as any}
-            key={product.id}
-          />
-        ))}
+      <div className="flex flex-col gap-5 overflow-hidden">
+        <ScrollArea className="h-full">
+          <div className="flex h-full flex-col gap-2">
+            {products.length > 0 ? (
+              products.map((product) => (
+                <CartItem
+                  product={computeProductTotalPrice(product as any) as any}
+                  key={product.id}
+                />
+              ))
+            ) : (
+              <p className="pt-2 text-center text-sm opacity-60">
+                Carrinho vazio
+              </p>
+            )}
+          </div>
+        </ScrollArea>
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <Separator />
+        <div className="div flex items-center justify-between text-xs">
+          <p>Subtotal</p>
+          <p>R$ {subTotal.toFixed(2)}</p>
+        </div>
+
+        <Separator />
+        <div className="div flex items-center justify-between text-xs">
+          <p>Entrega</p>
+          <p>GRÁTIS</p>
+        </div>
+
+        <Separator />
+        <div className="div flex items-center justify-between text-xs">
+          <p>Descontos</p>
+          <p>R$ {totalDiscount.toFixed(2)}</p>
+        </div>
+
+        <Separator />
+        <div className="div flex items-center justify-between text-xs font-bold">
+          <p>Total</p>
+          <p>R$ {total.toFixed(2)}</p>
+        </div>
+
+        <Button className="mt-7 font-bold uppercase">Finalizar comprar</Button>
       </div>
     </div>
   );
--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -2,7 +2,7 @@
 
 import { ProductWithTotalPrice } from "@/helpers/product";
 
-import { ReactNode, createContext, useState } from "react";
+import { ReactNode, createContext, useMemo, useState } from "react";
 
 export interface CartProduct extends ProductWithTotalPrice {
   quantity: number;
@@ -13,6 +13,9 @@ interface ICartContext {
   cartTotalPrice: number;
   cartBasePrice: number;
   cartTotalDiscount: number;
+  total: number;
+  subTotal: number;
+  totalDiscount: number;
   addProductToCart: (product: CartProduct) => void;
   decreaseProductQuantity: (productId: string) => void;
   increaseProductQuantity: (productId: string) => void;
@@ -24,6 +27,9 @@ export const CartContext = createContext<ICartContext>({
   cartTotalPrice: 0,
   cartBasePrice: 0,
   cartTotalDiscount: 0,
+  total: 0,
+  subTotal: 0,
+  totalDiscount: 0,
   addProductToCart: () => {},
   decreaseProductQuantity: () => {},
   increaseProductQuantity: () => {},
@@ -32,6 +38,22 @@ export const CartContext = createContext<ICartContext>({
 
 export function CartProvider({ children }: { children: ReactNode }) {
   const [products, setProducts] = useState<CartProduct[]>([]);
+
+  const subTotal = useMemo(() => {
+    return products.reduce((acc, product) => {
+      return acc + Number(product.basePrice) * product.quantity;
+    }, 0);
+  }, [products]);
+
+  const total = useMemo(() => {
+    return products.reduce((acc, product) => {
+      return acc + product.totalPrice * product.quantity;
+    }, 0);
+  }, [products]);
+
+  const totalDiscount = useMemo(() => {
+    return total - subTotal;
+  }, [total, subTotal]);
 
   const addProductToCart = (product: CartProduct) => {
     const productIsAlreadyOnCart = products.some(
@@ -103,6 +125,9 @@ export function CartProvider({ children }: { children: ReactNode }) {
         cartTotalPrice: 0,
         cartBasePrice: 0,
         cartTotalDiscount: 0,
+        total,
+        subTotal,
+        totalDiscount,
         addProductToCart,
         decreaseProductQuantity,
         increaseProductQuantity,
